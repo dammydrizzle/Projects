20440d397a33e388fd048300a53b7c08b38e9e7a
Adedokun Ishaq Damilola <dammydrizzle@gmail.com>
2023-08-07 17:41:51 +0100

    added another variable resultElement

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const btnElement = document.getElementById("btn")
 
 const birthdayElement = document.getElementById("birthday")
 
+const resultElement = document.getElementById("result")
+
 function calculateAge() {
    const birthdayValue = birthdayElement.value;
    
    if(birthdayValue === "") {
     alert("Please enter your birthday")
+   } else {
+    const age = getAge(birthdayValue)
+    console.log(age)
+    
    }
    } 
+
+   function getAge(birthdayValue) {
+    const currentDate = new Date()
+    const birthdayDate = new Date(birthdayValue)
+    
+    let age = currentDate.getFullYear()-birthdayDate.getFullYear()
+    const month = currentDate.getMonth()-birthdayDate.getMonth()
+   
+    if(month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())) {
+        age--
+    }
+
+    return age
+    
+
+   }
    
 
 
